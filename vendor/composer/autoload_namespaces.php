e201b96dc9876828c72014b390d3298dc60dc7fd
EDDYCJY <313687982@qq.com>
2018-10-27 17:39:43 +0800

    init gRPC PHP Server

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,9 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
-    'Diff' => array($vendorDir . '/phpspec/php-diff/lib'),
-    'Codeception\\' => array($vendorDir . '/codeception/specify/src'),
-    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
 );
